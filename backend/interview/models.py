a72d6509f959fce5a05161dff34139d59d496186
Raj <dhariwalraj37@gmail.com>
2026-03-27 11:26:57 +0530

    change in serilizer.py

--- backend/interview/models.py
+++ backend/interview/models.py
@@ -25,3 +25,6 @@ class Answer(models.Model):
 
     def __str__(self):
         return f"{self.user.username} - {self.score}"
+     def _str_(self):
+            return f"{self.user.username}-{self.score}"
+            
